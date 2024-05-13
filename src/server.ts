4d4523eb8874d4f31e46b6216bfb01147fd0ad40
Margus Liinev <margus.liinev@gmail.com>
2024-05-13 19:41:36 +0300

    add eslint, prettier, update tsconfig

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import Fastify from 'fastify';
 
 const app = Fastify();
 
-app.get('/', async function handler() {
+app.get('/', function handler() {
     return { hello: 'world' };
 });
 
-app.listen({ port: 5000 });
+await app.listen({ port: 5000 });
