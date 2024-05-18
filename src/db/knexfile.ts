2bc2c36f8c28c875f5309559a3a056c02513e429
Margus Liinev <margus.liinev@gmail.com>
2024-05-18 20:05:23 +0300

    add docker and knex

---
+++ src/db/knexfile.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import type { Knex } from 'knex';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config: Knex.Config = {
+    client: 'mysql2',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: resolve(__dirname, './migrations'),
+        loadExtensions: ['.ts']
+    },
+    seeds: {
+        directory: resolve(__dirname, './seeds'),
+        loadExtensions: ['.ts']
+    }
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ await app.register(taskRoutes, { prefix: '/api/tasks' });
 
 const start = async () => {
     try {
-        await app.listen({ port: 5000 });
+        await app.listen({ port: Number(process.env.PORT) || 5000 });
     } catch (err) {
         app.log.error('Error starting server:', err);
         process.exit(1);
