100a6c3d4fdcbf1b523152f02ccbf200f64325a3
Margus Liinev <margus.liinev@gmail.com>
2024-05-19 23:56:12 +0300

    remove dotenv package, read env variables with node core module

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,13 +1,9 @@
-import dotenv from 'dotenv';
 import type { Knex } from 'knex';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const envPath = resolve(__dirname, '../../.env');
-dotenv.config({ path: envPath });
 
 const config: Knex.Config = {
     client: 'mysql2',
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import Fastify from 'fastify';
 import UsersRoute from './routes/users.js';
 
