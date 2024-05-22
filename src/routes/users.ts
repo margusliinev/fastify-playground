fcfe365793331075f315861e717ed1dab86dd941
Margus Liinev <margus.liinev@gmail.com>
2024-05-22 22:00:19 +0300

    separate validation, serialization to schemas folder

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,66 +1,14 @@
 import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import { getUsersSchema, postUsersSchema } from '@/schemas/userSchemas.js';
 import { User } from '@/types/index.js';
 
-// TEMPORARY SOLUTION
-
-const newUserSchema = {
-    type: 'object',
-    properties: {
-        id: { type: 'number' },
-        username: { type: 'string', minLength: 3 },
-        email: { type: 'string', format: 'email' },
-        password: { type: 'string', minLength: 8 },
-        created_at: { type: 'string' },
-        updated_at: { type: 'string' }
-    },
-    required: ['username', 'email', 'password'],
-    additionalProperties: false
-};
-
-const getUsersResponse = {
-    type: 'object',
-    properties: {
-        success: { type: 'boolean' },
-        data: { type: 'array', items: { type: 'object', properties: newUserSchema.properties } }
-    }
-} as const;
-
-const postUsersResponse = {
-    type: 'object',
-    properties: {
-        success: { type: 'boolean' },
-        data: { type: 'object', properties: newUserSchema.properties }
-    }
-} as const;
-
-const tags = ['Users'] as const;
-
-const getUsersConfig = {
-    schema: {
-        tags,
-        response: {
-            200: getUsersResponse
-        }
-    }
-} as const;
-
-const postUsersConfig = {
-    schema: {
-        body: newUserSchema,
-        tags,
-        response: {
-            201: postUsersResponse
-        }
-    }
-} as const;
-
 function usersRouter(app: FastifyInstance, _opts: FastifyPluginOptions, done: Function) {
-    app.get('/users', getUsersConfig, async (_request, reply) => {
+    app.get('/users', getUsersSchema, async (_request, reply) => {
         const users = await app.db.select<User[]>('*').from('users');
         await reply.send({ success: true, data: users });
     });
 
-    app.post('/users', postUsersConfig, async (request, reply) => {
+    app.post('/users', postUsersSchema, async (request, reply) => {
         const { username, email, password } = request.body as Partial<User>;
         const [newUserID] = await app.db('users').insert({ username, email, password });
         const newUser = await app.db<User>('users').where('id', newUserID).first();
---
+++ src/schemas/userSchemas.ts
@@ -0,0 +1,52 @@
+const tag = ['Users'] as const;
+
+const newUser = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        username: { type: 'string', minLength: 3 },
+        email: { type: 'string', format: 'email' },
+        password: { type: 'string', minLength: 8 },
+        created_at: { type: 'string', format: 'date-time' },
+        updated_at: { type: 'string', format: 'date-time' }
+    },
+    required: ['username', 'email', 'password'],
+    additionalProperties: false
+} as const;
+
+const getUsersResponse = {
+    type: 'object',
+    properties: {
+        success: { type: 'boolean' },
+        data: { type: 'array', items: { type: 'object', properties: newUser.properties } }
+    }
+} as const;
+
+const postUsersResponse = {
+    type: 'object',
+    properties: {
+        success: { type: 'boolean' },
+        data: { type: 'object', properties: newUser.properties }
+    }
+} as const;
+
+const getUsersSchema = {
+    schema: {
+        tags: tag,
+        response: {
+            200: getUsersResponse
+        }
+    }
+} as const;
+
+const postUsersSchema = {
+    schema: {
+        tags: tag,
+        body: newUser,
+        response: {
+            201: postUsersResponse
+        }
+    }
+} as const;
+
+export { getUsersSchema, postUsersSchema };
